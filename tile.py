e6e984b27318e6ce817f86ef547b4b2c9286effc
Kerruan <sittigmarius@tuta.io>
2021-06-03 00:13:37 +0200

    Small change to tiles

--- tile.py
+++ tile.py
@@ -1,5 +1,7 @@
-class Tile:
+from model.AbstractObject import AbstractObject
+class Tile(AbstractObject):
 	def __init__(self, x, y, tile_type):
+		super().__init__(tile_type)
 		self.x = x
 		self.y = y
 		self.tile_type = tile_type
